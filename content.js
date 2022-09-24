4946a8419d89801d9d4b7ad2a596a4f84ab0e672
AlejoVinluan <alejo.vinluan146@gmail.com>
2022-09-24 03:30:13 -0500

    Working with definitions

---
+++ content.js
@@ -0,0 +1,19 @@
+window.addEventListener("mouseup", handleSelection);
+
+var highlightedText;
+
+function handleSelection() {
+  highlightedText = window.getSelection().toString().replace(/\s/g, "");
+}
+
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message, sender, sendResponse) {
+    let msg;
+    if (highlightedText.length <= 0){
+        msg = highlightedText = "NO_TEXT"
+    } else {
+        msg = highlightedText
+    }
+  sendResponse({ sent_msg: msg });
+}
--- popup.js
+++ popup.js
@@ -1,6 +1,74 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+let query = { active: true, currentWindow: true };
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+chrome.tabs.query(query, gotTabs);
+function gotTabs(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, {txt: "_"}, function (response) {
+    if (!response) {
+      document.getElementById("phonetic").innerHTML = "HelpLaw";
+      document.getElementById("example").innerHTML = "Defining and translating judicial words";
+    } else if (response.sent_msg === "NO_TEXT") {
+      document.getElementById("error").innerHTML = "Please highlight a word, then click the extension";
+    } else {
+      dictionary(response.sent_msg.replace(/[^a-zA-Z ]/g, ""));
+    }
+  });
+}
+
+let wordef, word, phonetic,pos,defin, example, sourceurl, index = 0, indlimit;
+
+async function dictionary(query) {
+  let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${query}`;
+  let response = await fetch(url);
+  wordef = await response.json();
+  if (wordef && !wordef.title) {
+    indlimit = wordef[0].meanings.length;
+    word = wordef[0].word;
+    phonetic = wordef[0].phonetic ? wordef[0].phonetic : "";
+    sourceurl = `https://en.wiktionary.org/wiki/${word}`;
+    index = 0;
+
+    setValues();
+
+    if (indlimit > 1) {
+      document
+        .getElementById("navigatecontainer")
+        .classList.remove("hidenavigator");
+    }
+  } else if (wordef.title) {
+    document.getElementById("error").innerHTML = "⚠  " + wordef.title;
+  }
+}
+
+document.getElementById("prev").addEventListener("click", handlePrevious);
+document.getElementById("next").addEventListener("click", handleNext);
+
+function handlePrevious() {
+  index = index - 1;
+  if (index < 0) index = indlimit - 1;
+  setValues();
+}
+
+function handleNext() {
+  index = index + 1;
+  if (index >= indlimit) index = 0;
+  setValues();
+}
+
+function setValues() {
+  pos = wordef[0].meanings[index].partOfSpeech;
+  defin = wordef[0].meanings[index].definitions[0].definition;
+  example = wordef[0].meanings[index].definitions[0].example
+    ? wordef[0].meanings[index].definitions[0].example
+    : null;
+
+  document.getElementById(
+    "word"
+  ).innerHTML = `${word} <a href=${sourceurl} class="searchanchor" target="_blank"><img class="searchsvg" title="read more" src = "../assets/searchonweb.svg" alt="read more"/><a>`;
+  document.getElementById("phonetic").innerHTML = `${phonetic}  (${pos})`;
+  document.getElementById("definition").innerHTML = defin;
+  if (example) {
+    document.getElementById("example").innerHTML = `Example: ${example}`;
+  } else {
+    document.getElementById("example").innerHTML = "";
+  }
+}
